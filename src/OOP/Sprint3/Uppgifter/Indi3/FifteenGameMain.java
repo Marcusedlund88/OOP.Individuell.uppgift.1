6d5c71e4a4591411dee00588e849bb9efea7d5c4
Marcus Edlund <marcus.edlund88@gmail.com>
2022-10-30 15:44:51 +0100

    OOP.Assignment1

---
+++ src/OOP/Sprint3/Uppgifter/Indi3/FifteenGameMain.java
@@ -0,0 +1,12 @@
+package OOP.Sprint3.Uppgifter.Indi3;
+
+public class FifteenGameMain {
+
+    public static void main(String[] args){
+
+        MyGameFrame frame = new MyGameFrame();
+
+
+    }
+
+}
